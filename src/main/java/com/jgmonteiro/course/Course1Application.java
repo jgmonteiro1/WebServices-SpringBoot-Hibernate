da9328fc0a158d74710f896b82d93ff15f1e6761
João Gabriel <jgmoreira15@gmail.com>
2019-12-19 23:16:12 -0300

    Projeto criado

---
+++ src/main/java/com/jgmonteiro/course/Course1Application.java
@@ -0,0 +1,13 @@
+package com.jgmonteiro.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course1Application.class, args);
+	}
+
+}
