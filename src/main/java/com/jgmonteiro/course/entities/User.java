0552984752f6d74c8738b5cecbd01de7a87d85db
João Gabriel <jgmoreira15@gmail.com>
2020-01-12 21:28:14 -0300

    Criação da entidade de pedidos, relacionamento entre os pedidos e os clientes e implementação dos mapeamentos da associação

--- src/main/java/com/jgmonteiro/course/entities/User.java
+++ src/main/java/com/jgmonteiro/course/entities/User.java
@@ -1,12 +1,17 @@
 package com.jgmonteiro.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-@Entity							
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+@Entity	
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 
@@ -18,6 +23,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User () {
 		
@@ -70,6 +78,10 @@ public class User implements Serializable {
 	public String getPassword() {
 		return password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -107,6 +119,8 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 	
 }
