47509168699564f6287d0b871170f8e3090fb578
João Gabriel <jgmoreira15@gmail.com>
2020-02-07 20:47:00 -0300

    Criação do método Update na classe UserService e UserRersouce

--- src/main/java/com/jgmonteiro/course/services/UserServices.java
+++ src/main/java/com/jgmonteiro/course/services/UserServices.java
@@ -30,4 +30,16 @@ public class UserServices {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		upadateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void upadateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
